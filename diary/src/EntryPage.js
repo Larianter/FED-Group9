012c5cf911e70c0f7a687f18e249d9a103792b8e
Larianter <lari.vainio@edu.turkuamk.fi>
2025-04-24 19:45:20 +0300

    created "entry page" and added basic routing functionality

--- diary/src/EntryPage.js
+++ diary/src/EntryPage.js
@@ -2,16 +2,16 @@ import React from "react";
 import  "./DiaryStyleSheet.css";
 import { useNavigate } from "react-router-dom";
 
- function DiaryPageUI() {
+function EntryPageUI() {
     const navigate = useNavigate();
-    const navigateToEntry = () => {navigate('/entries')}
+    const navigateToDiary = () => {navigate('/')}
 
     return (
         <div className="diary-container">
             
         {/*Header*/}
         <div className="diary-header">
-            <button className="diary-title" onClick={navigateToEntry}>Diary_name</button>
+            <button className="diary-title" onClick={navigateToDiary}>Diary Entries</button>
             <div className="customize-section">
                 <button>Customize page</button>
                 <span className="color-indicator"></span>
@@ -25,19 +25,8 @@ import { useNavigate } from "react-router-dom";
                 <div className="right-page"></div>
             </div>
         </div>
-
-        {/*Arrow button*/}
-        <button className="arrow-button">→</button>
-
-        {/*Sidebar*/}
-        <div className="sidebar-button-container">
-            <button className="sidebar-button">T</button>
-            <button className="sidebar-button">🖼</button>
-            <button className="sidebar-button">M</button>
-            <button className="sidebar-button">...</button>
-        </div>
     </div>
    );
 }
 
-export default DiaryPageUI;
+export default EntryPageUI;
--- diary/src/index.js
+++ diary/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import DiaryPageUI from './DiaryPage';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DiaryPageUI />
+    <App />
   </React.StrictMode>
 );
 
